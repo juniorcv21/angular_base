34d7b3c5a7811af97f81c6566d85749bb491cbb8
Carlos <ctangcontreras@gmail.com>
2022-04-08 16:57:42 -0500

    cambio scss

--- src/app/service/ejemplo.ts
+++ src/app/service/ejemplo.ts
@@ -1,3 +1,6 @@
+//***************************   SERVICE     ************************* */
+
+// --------------->  TS
 /* import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
@@ -31,3 +34,44 @@ export class SolPlantacionForestalGeometriaService {
   }
 }
  */
+
+/************************** DIALOG (ACEPTAR) ****************************** */
+
+// --------------->  HTML
+/* <p-dialog header="AVISO"
+    [(visible)]="geeks" [modal]="true"
+    [draggable]="false" [resizable]="false">
+
+    <p class="p-m-0"> <li class="pi pi-exclamation-triangle"></li>&nbsp;
+        La no presentación de subsanaciones dentro del plazo hará considerar el documento de gestión (PMFI/DEMA) como "No presentado" conforme al literal 4 del articulo 136 del TUO de la LGPA ley 27444.
+    </p>
+    <ng-template pTemplate="footer">
+        <p-button icon="pi pi-check"
+            (click)="geeks=false" label="Aceptar"
+            class="p-button-text">
+        </p-button>
+    </ng-template>
+</p-dialog> */
+
+/************************* CONFIRM DIALOG ( SI | NO)  */
+// --------------->  TS
+/* constructor(
+  private confirmationService: ConfirmationService,
+){} */
+
+// --------------->  TS
+/* mostrarAviso() {
+  this.confirmationService.confirm({
+    message: 'La no presentación de subsanaciones dentro del plazo hará considerar el documento de gestión (PMFI/DEMA) como "No presentado" conforme al literal 4 del articulo 136 del TUO de la LGPA ley 27444.',
+    icon: 'pi pi-exclamation-triangle',
+    acceptLabel: 'Aceptar',
+    accept: () => {
+      this.ref.close
+    },
+
+  });
+
+} */
+
+// --------------->  HTML
+/* <p-confirmDialog [style]="{ width: '300px' }" ></p-confirmDialog> */
